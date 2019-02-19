698bf171846ee1b3c2890f2a20f3c396d4bcfe93
Thomas Rayden <thomasmwrayden@gmail.com>
2019-02-19 19:59:23 +0800

    Handle environment variables

--- src/main.go
+++ src/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/caarlos0/env"
+)
+
+// Config for Environment variables
+type Config struct {
+	Home         string `env:"HOME"`
+	Port         int    `env:"PORT" envDefault:"3000"`
+	IsProduction bool   `env:"PRODUCTION"`
+}
+
+func main() {
+	cfg := Config{}
+	env.Parse(&cfg)
+	fmt.Println(cfg)
 }
